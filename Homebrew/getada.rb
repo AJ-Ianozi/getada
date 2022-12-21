8fce1c2e155b67f1af301aa9f01290d266257c85
Jesper Quorning <jesper.quorning@gmail.com>
2022-12-22 00:56:28 +0100

    Skeleton - WIP

--- Homebrew/getada.rb
+++ Homebrew/getada.rb
@@ -1,17 +1,32 @@
-puts "GetAda -- The Homebrew Ada installer for macOS                           "
-puts "--                                                                       "
-puts "tl;dr;                                                                   "
-puts "----                                                                     "
-puts " > getada install GNAT        -- Install the GNAT compiler and tools     "
-puts " > getada install GPRBuild    -- Install the GPRBuild GNAT Project tools "
-puts " > getada install GNATStudio  -- Install the GNAT Studio IDE             "
-puts " > getada install Alire       -- Install the `alr` Ada packaging tools   "
-puts "                                                                         "
-puts "Usage                                                                    "
-puts "----                                                                     "
-puts "   getada install [ GNAT | GPRBuild | GNATStudio | Alire ]               "
 
-name = gets.chop
-puts "The name #{name} was entered"
+#!env_rb ruby
+#
+# license: MIT
+
+display_app     = "GetAdaNow"
+app             = display_app.downcase
+display_install = "Install"
+install         = display_install.downcase
+
+puts ""
+puts "#{display_app} -- The Homebrew Ada installer for macOS                      "
+puts "--                                                                          "
+puts "tl;dr;                                                                      "
+puts "----                                                                        "
+puts " > :app :env setup"
+puts " > :app :install .all"
+puts "Usage 1"
+puts "----"
+puts " > #{app} #{install} GNAT        -- #{display_install} the GNAT compiler and tools     "
+puts " > #{app} #{install} GPRBuild    -- #{display_install} the GPRBuild GNAT Project tools "
+puts " > #{app} #{install} GNATStudio  -- #{display_install} the GNAT Studio IDE             "
+puts " > #{app} #{install} Alire       -- #{display_install} the Alire packaging tools   "
+puts " > #app #environment check"
+puts " > #app #environment setup"
+puts "                                                                            "
+puts "Usage                                                                       "
+puts "----                                                                        "
+puts "   #{app} #{install} [ GNAT | GPRBuild | GNATStudio | Alire ]               "
+
 
 
