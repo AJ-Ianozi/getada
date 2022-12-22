7abd709d20494fab9b45008bfd775caf4a7772a5
Jesper Quorning <jesper.quorning@gmail.com>
2022-12-22 01:54:54 +0100

    Test 47 about mapping dir to lowercase

--- Homebrew/getada.rb
+++ Homebrew/getada.rb
@@ -10,7 +10,16 @@ install = Install.downcase
 Env     = "Environment"
 env     = Env.downcase
 
-environment = env
+CMD = [ :env, :inst ]
+Cmd = [ :env => "Environment", :inst => "Install" ]
+
+cmd = Cmd.each do | k, v |
+   k.to_s.downcase
+end
+
+puts cmd
+
+# environment = env
 
 puts ""
 puts "#{App} -- The Homebrew Ada installer for macOS                      "
@@ -18,21 +27,20 @@ puts "--                                                                        
 puts ""
 puts "tl;dr;                                                                      "
 puts "----                                                                        "
-puts " > #{app} #{environment} setup"
+puts " > #{app} #{env} setup"
 puts " > #{app} #{install} .all"
-puts ""
+puts "or simply"
+puts " > #{app} setup.all"
+puts
 puts "The not so short version"
 puts "----"
+puts " > #{app} #{env} check"
+puts " > #{app} #{env} setup"
 puts " > #{app} #{install} GNAT        -- #{Install} the GNAT compiler and tools     "
 puts " > #{app} #{install} GPRBuild    -- #{Install} the GPRBuild GNAT Project tools "
 puts " > #{app} #{install} GNATStudio  -- #{Install} the GNAT Studio IDE             "
 puts " > #{app} #{install} Alire       -- #{Install} the Alire packaging tools   "
-puts " > #{app} #{environment} check"
-puts " > #{app} #{environment} setup"
-puts "                                                                            "
+puts
 puts "Usage                                                                       "
 puts "----                                                                        "
 puts "   #{app} #{install} [ GNAT | GPRBuild | GNATStudio | Alire ]               "
-
-
-
