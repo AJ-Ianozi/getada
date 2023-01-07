a7127b9eec3955a8f5623d8b524b0e7a8ef57b5c
Anthony Ianozi <aj@ianozi.com>
2023-01-07 13:06:17 -0500

    Added license to file

--- src/defaults.ads
+++ src/defaults.ads
@@ -1,3 +1,20 @@
+--    Copyright (C) 2022 A.J. Ianozi <aj@ianozi.com>
+--
+--    This file is part of GetAda: the Unofficial Alire Installer
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 with Ada.Characters.Latin_1;
 package Defaults is
    --  Directories, starting at $HOME
